0fed971754fc30b0b09fffc06c2c26579343c78d
lotfi <lotfi.amin@hotmail.com>
2024-01-30 09:56:30 +0100

    hj

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: "/landing-page",
+    images: { unoptimized: true },
+    experimental: { images: { unoptimized: true } },
 };
 
 export default nextConfig;
